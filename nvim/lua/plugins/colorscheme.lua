eefa2d005f825cb12b18598f1e83913e77ef8306
seirennn <lansojordan7@gmail.com>
2025-02-22 04:52:50 +0530

    .

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
-      require("gruvbox").setup({
-        contrast = "hard", -- Set contrast option
-        palette_overrides = { dark0_hard = "#0E1018" }, -- Custom dark background
+      require('gruvbox').setup({
+        contrast = 'hard', -- Set contrast option
+        palette_overrides = { dark0_hard = '#0E1018' }, -- Custom dark background
         -- Customize overrides...
         overrides = {
-          NormalFloat = { fg = "#ebdbb2", bg = "#504945" },
-          Comment = { fg = "#81878f", italic = true, bold = true },
+          NormalFloat = { fg = '#ebdbb2', bg = '#504945' },
+          Comment = { fg = '#81878f', italic = true, bold = true },
           -- Add other overrides as needed...
         },
       })
-      vim.cmd("colorscheme gruvbox")
+      vim.cmd('colorscheme gruvbox')
     end,
   },
 }
